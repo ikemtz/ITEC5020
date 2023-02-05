ad6882731299fe46b8710ab630f550eadf729b98
Isaac Martinez (@ikemtz) <ikemtz@outlook.com>
2023-02-05 18:27:50 -0500

    Fixing validation

--- CapestraApp/src/CapestraApp/BaseDataEntryUI.java
+++ CapestraApp/src/CapestraApp/BaseDataEntryUI.java
@@ -1,14 +1,27 @@
- 
 package CapestraApp;
 
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 /**
  *
- * @author Ikemtz * 
- * Simple base class to ensure commonality across all *DataEntryUI classes
+ * @author Ikemtz * Simple base class to ensure commonality across all
+ *         *DataEntryUI classes
  */
 public abstract class BaseDataEntryUI extends BaseUI {
-    //This label will provide the DB feedback mechanism for all data entry screens
-        public Label statusLBL = new Label("");
+
+  // This label will provide the DB feedback mechanism for all data entry screens
+  public Label statusLBL = new Label("");
+
+  public BaseDataEntryUI() {
+    statusLBL.setWrapText(true);
+  }
+
+  public void updateStatusLabel(boolean hasErrors) {
+    if (hasErrors) {
+      statusLBL.setTextFill(Color.RED);
+    } else {
+      statusLBL.setTextFill(Color.BLACK);
+    }
+  }
 }
--- CapestraApp/src/CapestraApp/CustomerDataEntryUI.java
+++ CapestraApp/src/CapestraApp/CustomerDataEntryUI.java
@@ -7,109 +7,115 @@ import javafx.scene.layout.VBox;
 
 public class CustomerDataEntryUI extends BaseDataEntryUI {
 
-    private HBoxAndControl firstnameHBC, lastnameHBC, addressHBC, cityHBC, stateHBC, zipHBC, phoneHBC, emailHBC;
+  private HBoxAndControl firstnameHBC, lastnameHBC, addressHBC, cityHBC, stateHBC, zipHBC, phoneHBC, emailHBC;
 
-    // Add Customer Scene Setup
-    @Override
-    public Scene createScene(MenuBar menuBar) {
+  // Add Customer Scene Setup
+  @Override
+  public Scene createScene(MenuBar menuBar) {
 
-        firstnameHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "First Name", 150);
-        lastnameHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "Last Name", 150);
-        addressHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "Address", 210);
-        cityHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "City", 100);
-        stateHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "State", 90);
-        zipHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "Zip", 75);
-        phoneHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "Phone", 100);
-        emailHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "Email", 150);
+    firstnameHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "First Name", 150);
+    lastnameHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "Last Name", 150);
+    addressHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "Address", 210);
+    cityHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "City", 100);
+    stateHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "State", 90);
+    zipHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "Zip", 75);
+    phoneHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "Phone", 100);
+    emailHBC = UiFactory.createHBoxAndControl(ControlTypes.TextField, "Email", 150);
 
-        Button addCustBTN = new Button("Add Customer");
+    Button addCustBTN = new Button("Add Customer");
 
-        addCustBTN.setOnAction(e -> {
-            addCustomerToDb();
-        });
+    addCustBTN.setOnAction(e -> {
+      addCustomerToDb();
+    });
 
-        Button clearBTN = new Button("Clear");
-        clearBTN.setOnAction(e -> {
-            clearData();
-        });
+    Button clearBTN = new Button("Clear");
+    clearBTN.setOnAction(e -> {
+      clearData();
+    });
 
-        VBox vbox = new VBox(15,
-                createTitleHBox("Add Customer"),
-                firstnameHBC.getHBox(),
-                lastnameHBC.getHBox(),
-                addressHBC.getHBox(),
-                cityHBC.getHBox(),
-                stateHBC.getHBox(),
-                zipHBC.getHBox(),
-                phoneHBC.getHBox(),
-                emailHBC.getHBox(),
-                UiFactory.createCommandHBox(addCustBTN, clearBTN),
-                statusLBL);
-        vbox.setPadding(UiFactory.DEFAULT_INSETS);
+    VBox vbox = new VBox(15,
+        createTitleHBox("Add Customer"),
+        firstnameHBC.getHBox(),
+        lastnameHBC.getHBox(),
+        addressHBC.getHBox(),
+        cityHBC.getHBox(),
+        stateHBC.getHBox(),
+        zipHBC.getHBox(),
+        phoneHBC.getHBox(),
+        emailHBC.getHBox(),
+        UiFactory.createCommandHBox(addCustBTN, clearBTN),
+        statusLBL);
+    vbox.setPadding(UiFactory.DEFAULT_INSETS);
 
-        VBox vboxOL = new VBox(menuBar, vbox);
+    VBox vboxOL = new VBox(menuBar, vbox);
 
-        return new Scene(vboxOL, 600, 500);
+    return new Scene(vboxOL, 600, 500);
+  }
+
+  // This function clears the data
+  public void clearData() {
+    firstnameHBC.setText("");
+    lastnameHBC.setText("");
+    addressHBC.setText("");
+    cityHBC.setText("");
+    stateHBC.setText("");
+    zipHBC.setText("");
+    phoneHBC.setText("");
+    emailHBC.setText("");
+  }
+
+  // Add a (new) customer to the DB
+  public void addCustomerToDb() {
+    Customer customer = new Customer(0,
+        firstnameHBC.getText(),
+        lastnameHBC.getText(),
+        addressHBC.getText(),
+        cityHBC.getText(),
+        stateHBC.getText(),
+        zipHBC.getText(),
+        phoneHBC.getText(),
+        emailHBC.getText());
+    if (customerIsValid(customer)) {
+      boolean customerAdded = getMyDB().addCustomer(customer, statusLBL);
+      this.updateStatusLabel(!customerAdded);
+      if (customerAdded) {
+        clearData();
+      }
     }
+  } // end addCustomer()
 
-    //This function clears the data
-    public void clearData() {
-        firstnameHBC.setText("");
-        lastnameHBC.setText("");
-        addressHBC.setText("");
-        cityHBC.setText("");
-        stateHBC.setText("");
-        zipHBC.setText("");
-        phoneHBC.setText("");
-        emailHBC.setText("");
+  // Ensures that all customer properties are valid
+  private boolean customerIsValid(Customer customer) {
+    StringBuilder errorMessages = new StringBuilder();
+    if (customer.getFirstName().isEmpty()) {
+      errorMessages.append("First Name is required; ");
     }
-
-    // Add a (new) customer to the DB 
-    public void addCustomerToDb() {
-        Customer customer = new Customer(0,
-                firstnameHBC.getText(),
-                lastnameHBC.getText(),
-                addressHBC.getText(),
-                cityHBC.getText(),
-                stateHBC.getText(),
-                zipHBC.getText(),
-                phoneHBC.getText(),
-                emailHBC.getText());
-        if (customerIsValid(customer)) {
-            boolean customerAdded = getMyDB().addCustomer(customer, statusLBL);
-            if (customerAdded) {
-                clearData();
-            }
-        }
-    } // end addCustomer()
-
-    //Ensures that all customer properties are valid
-    private boolean customerIsValid(Customer customer) {
-        if (customer.getFirstName().isEmpty()) {
-            this.statusLBL.setText("ERROR: First Name is required.");
-            return false;
-        } else if (customer.getLastName().isEmpty()) {
-            this.statusLBL.setText("ERROR: Last Name is required.");
-            return false;
-        } else if (customer.getAddress().isEmpty()) {
-            this.statusLBL.setText("ERROR: Address is required.");
-            return false;
-        } else if (customer.getCity().isEmpty()) {
-            this.statusLBL.setText("ERROR: City is required.");
-            return false;
-        } else if (customer.getState().isEmpty()) {
-            this.statusLBL.setText("ERROR: State is required.");
-            return false;
-        } else if (customer.getZip().isEmpty()) {
-            this.statusLBL.setText("ERROR: Zip is required.");
-            return false;
-        } else if (customer.getPhone().isEmpty()) {
-            this.statusLBL.setText("ERROR: Phone is required.");
-            return false;
-        } else if (customer.getEmail().isEmpty()) {
-            this.statusLBL.setText("ERROR: Email is required.");
-            return false;
-        }
-        return true;
+    if (customer.getLastName().isEmpty()) {
+      errorMessages.append("Last Name is required; ");
     }
+    if (customer.getAddress().isEmpty()) {
+      errorMessages.append("Address is required; ");
+    }
+    if (customer.getCity().isEmpty()) {
+      errorMessages.append("City is required; ");
+    }
+    if (customer.getState().isEmpty()) {
+      errorMessages.append("State is required; ");
+    }
+    if (customer.getZip().isEmpty()) {
+      errorMessages.append("Zip is required; ");
+    }
+    if (customer.getPhone().isEmpty()) {
+      errorMessages.append("Phone is required; ");
+    }
+    if (customer.getEmail().isEmpty()) {
+      errorMessages.append("Email is required; ");
+    }
+    boolean hasErrors = errorMessages.length() > 0;
+    if (hasErrors) {
+      this.statusLBL.setText("ERROR: " + errorMessages.toString().trim());
+    }
+    this.updateStatusLabel(hasErrors);
+    return !hasErrors;
+  }
 }
